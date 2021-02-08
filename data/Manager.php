e4bc57a605abd2440a4e9b04f448e84f0944db10
andikajayaw <andikajayawiguna@gmail.com>
2021-02-08 22:29:05 +0800

    Construct Overriding

--- data/Manager.php
+++ data/Manager.php
@@ -2,14 +2,27 @@
 
 class Manager {
     var string $name;
+    var string $title;
 
-    function sayHello(string $name) {
+    public function __construct(string $name = '', string $title = 'Manager') 
+    {
+        $this->name = $name;
+        $this->title = $title;
+    }
+
+    function sayHello(string $name) :void {
         echo "Hi $name, Manager name is $this->name" . PHP_EOL;
     }
 }
 
 class VicePresident extends Manager {
-    function sayHello(string $name) {
+    public function __construct(string $name = '')
+    {
+        // Tidak wajib, tapi direkomendasikan
+        parent::__construct($name, 'VP');
+    }
+
+    function sayHello(string $name) :void {
         echo "Hi $name, VP name is $this->name" . PHP_EOL;
     }
 }
